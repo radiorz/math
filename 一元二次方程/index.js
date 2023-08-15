a7f7c78c725f1dc8fc3c8dcc0527a77883f92feb
tikkhun <s565338107@163.com>
2023-08-15 19:10:59 +0800

    feat: 可以求解一次二元方程

--- 一元二次方程/index.js
+++ 一元二次方程/index.js
@@ -1,31 +1,35 @@
-const args = {
-  a: 1,
-  b: 2,
-  c: 3,
-};
-// a b c  分别为二次项系数 一次项系数 和 常数项系数
-const a = Number(args.a);
-const b = Number(args.b);
-const c = Number(args.c);
+/**
+ * 求解二元一次方程的根
+ * @param {number} a 二次项系数
+ * @param {number} b 一次项系数
+ * @param {number} c 常数项系数
+ */
+function to根(a, b, c) {
+  const { x1, x2 } = is虚数根(a, b, c) ? to虚数根(a, b, c) : to实数根(a, b, c);
+  console.log("方程的根分别为：" + "x1=" + x1 + "      " + "x2=" + x2);
+}
+
 function is虚数根(a, b, c) {
   return b * b - 4 * a * c < 0;
 }
-function to虚数根(a,b,c){
-
-}
-function to实数根(a,b,c){
-  
-}
-
-if (is虚数根(a, b, c)) {
+function to虚数根(a, b, c) {
   // 使用公式法求解
   var x1 =
     (-b / 2) * a + "-" + (Math.pow(4 * a * c - b * b, 1 / 2) / 2) * a + "i";
   var x2 =
     (-b / 2) * a + "+" + (Math.pow(4 * a * c - b * b, 1 / 2) / 2) * a + "i";
-  alert("方程的虚根分别为：" + "x1=" + x1 + "      " + "x2=" + x2);
-} else {
+  return { x1, x2 };
+}
+function to实数根(a, b, c) {
   var x1 = ((-b - Math.pow(b * b - 4 * a * c, 1 / 2)) / 2) * a;
   var x2 = ((-b + Math.pow(b * b - 4 * a * c, 1 / 2)) / 2) * a;
-  alert("方程的实根分别为：" + "x1=" + x1 + "      " + "x2=" + x2);
+  return { x1, x2 };
 }
+
+const args = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+to根(...Object.values(args));
